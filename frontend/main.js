578b60428a04e443825fa9bec2c7a8028204d073
felipe gonzalez <phillnation@hotmail.com>
2024-09-27 14:57:40 -0400

    few changes to the main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const productionApiUrl = "https://getresumecounterr.azurewebsites.net/api/GetResumeCounter?code=96Vv91wgQ15Ni3otJXWODMCVBigqHxT_cnUbKhF9UxZCAzFu3Elxjw%3D%3D"
+const productionApiUrl = "https://getresumecounterr.azurewebsites.net/api/GetResumeCounter?code=96Vv91wgQ15Ni3otJXWODMCVBigqHxT_cnUbKhF9UxZCAzFu3Elxjw%3D%3D";
 const localfunctionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
